93c7bc8e8ff38921b8bfacf32ea9cfb37f4a4c4c
Jono Cabrera <cjonathan193@gmail.com>
2024-12-17 19:10:45 +1100

    Updated CSS Layout and Implemented a fully functioning Sign Up form

--- js/firebaseAuth.js
+++ js/firebaseAuth.js
@@ -17,13 +17,26 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-const signUp = document.getElementById('submitRegister');
+function showMessage(message, divId) {
+    var messageDiv = document.getElementById(divId);
+    
+    messageDiv.style.display = "block";
+    messageDiv.innerHTML = message;
+    messageDiv.style.opacity = 1;
+
+    setTimeout(function() {
+        messageDiv.style.opacity = 0;
+    }, 5000);
+}
+
+const signUp = document.getElementById('submitSignUp');
 signUp.addEventListener('click', (event) => {
     event.preventDefault();
     
-    const username = document.getElementById('username').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
+    const email = document.getElementById('registerEmail').value;
+    const password = document.getElementById('registerPassword').value;
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
 
     const auth = getAuth();
     const db = getFirestore();
@@ -35,5 +48,27 @@ signUp.addEventListener('click', (event) => {
             email: email,
             password: password
         };
+
+        showMessage('Account Created Successfully!', 'registerMessage');
+
+        const docRef = doc(db, "users", user.uid);
+
+        setDoc(docRef, userData)
+        .then(() => {
+            window.location.href = 'index.html';
+        })
+        .catch((error) => {
+            console.error("An error occurred while writing document", error);
+        });
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+
+        if (errorCode == 'auth/email-already-in-use') {
+            showMessage('Email already in use!', 'registerMessage');
+        }
+        else {
+            showMessage('Unable to create user, please try again later', 'registerMessage');
+        }
     })
 })
--- js/script.js
+++ js/script.js
@@ -1,103 +1,14 @@
- 
- 
- /* Not Working */
- /*
- // Your web app's Firebase configuration
- const firebaseConfig = {
-    apiKey: "AIzaSyA-Pb4QGKYAF_6tZ47Qf9gG5Kpx0d2bbWg",
-    authDomain: "login-page-with-firebase-a094b.firebaseapp.com",
-    projectId: "login-page-with-firebase-a094b",
-    storageBucket: "login-page-with-firebase-a094b.firebasestorage.app",
-    messagingSenderId: "131986400336",
-    appId: "1:131986400336:web:bb31923cb4fc53c5a4dd55"
-  };
+const signUpButton = document.getElementById('signUpButton');
+const signInButton = document.getElementById('signInButton');
+const signInForm = document.getElementById('signIn');
+const signUpForm = document.getElementById('signup');
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+signUpButton.addEventListener('click', function() {
+    signInForm.style.display = "none";
+    signUpForm.style.display = "block";
+});
 
-  // Initialize Variables
-  const auth = firebaseConfig.auth();
-  const database = firebaseConfig.database();
-
-  // Register function
-  function register() {
-    // Get all input fields
-    username = document.getElementById('username').value;
-    email = document.getElementById('email').value;
-    password = document.getElementById('password').value;
-
-    // Validate all input fields
-    if (validateEmail(email) == false || validatePassword(password) == false) {
-        alert('Email or Password is invalid');
-        return;  // Don't continue running the code
-    }
-    if (validateField(username) == false) {
-        alert('Username invalid');
-        return;
-    }
-
-    // Move on with auth
-    auth.createUserWithEmailAndPassword(email, password)
-    .then(function() {
-        // Declare a new user
-        var user = auth.currentUser;
-
-        // Add user to Firebase
-        var database_ref  = database.ref();
-
-        // Create user data
-        var user_data = {
-            username: username,
-            email: email,
-            password: password,
-            last_login: Date.now()
-        }
-
-        database_ref.child('users/' + user.uid).set(user_data);
-
-        alert('User has been created successfully');
-    })
-    .catch(function(error) {
-        // Firebase will use this to catch any errors
-        var error_code = error.code;
-        var error_message = error.message;
-
-        alert(error_message)
-    })
-}
-
-function validateEmail(email) {
-    expression = /^[^@]+@\w+(\.\w+)+\w$/.test(str);
-    if (expression.test(email) == true) {
-        // if email valid
-        return true;
-    }
-    else {
-        // if email not valid
-        return false;
-    }
-}
-
-function validatePassword(password) {
-    // Firebase can only accept length greater than 6
-    if (password < 6) {
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-
-function validateField(field) {
-    if (field == null) {
-        return false;
-    }
-
-    if (field.length <= 0) {
-        return false;
-    }
-    else {
-        return true;
-    }
-}
-*/
+signInButton.addEventListener('click', function() {
+    signInForm.style.display = "block";
+    signUpForm.style.display = "none";
+});
